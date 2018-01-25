79b866c9de59f146117034961d29058793434d2f
shway250 <jackson_duhon@yahoo.com>
2018-01-24 19:43:43 -0800

    added favicon and changed canvas element a bit

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ var server = app.listen(process.env.PORT|| 3000);
 var io = socket(server);
 
 io.sockets.on('connection', function(socket){
-  console.log("Connected to client", socket.id);
+  // console.log("Connected to client", socket.id);
 
   socket.on('mouse', function(data){
-    console.log("Received: 'mouse' " + data.x + " " +data.y);
+    // console.log("Received: 'mouse' " + data.x + " " +data.y);
     socket.broadcast.emit('serverMouse', data);//this is the event that get's broadcasted to everyone
   });
 });
--- public/script.js
+++ public/script.js
@@ -1,8 +1,13 @@
 var socket;
 
 function setup(){
-  createCanvas(500,500);
+  createCanvas(windowWidth-20,windowHeight);
   background(0);
+  for(let i = (windowWidth/4); i<windowWidth-(windowWidth/4); i+=30){
+    for(let j = 0; j<windowHeight; j+=30){
+      rect(i,j,20,20);
+    }
+  }
 
  socket = io.connect('http://localhost:3000');
 
@@ -14,21 +19,70 @@ function setup(){
 }
 
 function draw(){
-  //nothing here
+  // for(let i = 0; i<width; i+=30){
+  //   for(let j = 0; j<height; j+=30){
+  //     rect(i,j,30,30);
+  //   }
+  // }
 }
 
 function mouseDragged(){
-  fill(255);
+  // for(let i = 0; i<500; i+=30){
+  //   for(let j = 0; j<500; j+=30){
+  //     rect(i,j,30,30);
+  //   }
+  // }
+  fill(55);
   noStroke();
   ellipse(mouseX, mouseY, 15, 15);
-  sendMouse(mouseX, mouseY);
 }
 
-function sendMouse(xpos, ypos){
-  var data = {
-    x: xpos,
-    y: ypos
-  }
 
-  socket.emit('mouse', data);
-}
+// var socket;
+
+// function setup(){
+//   createCanvas(500,500);
+//   // background(0);
+//   for(let i = 0; i<500; i+=30){
+//     for(let j = 0; j<500; j+=30){
+//       rect(i,j,30,30);
+//     }
+//   }
+
+//  socket = io.connect('http://localhost:3000');
+
+//  socket.on('serverMouse', function(data){
+//   fill(0,0,255);
+//   noStroke();
+//   ellipse(data.x, data.y, 15, 15);
+//  });
+// }
+
+// function draw(){
+//   // for(let i = 0; i<width; i+=30){
+//   //   for(let j = 0; j<height; j+=30){
+//   //     rect(i,j,30,30);
+//   //   }
+//   // }
+// }
+
+// function mouseDragged(){
+//   // for(let i = 0; i<500; i+=30){
+//   //   for(let j = 0; j<500; j+=30){
+//   //     rect(i,j,30,30);
+//   //   }
+//   // }
+//   fill(55);
+//   noStroke();
+//   ellipse(mouseX, mouseY, 15, 15);
+//   sendMouse(mouseX, mouseY);
+// }
+
+// function sendMouse(xpos, ypos){
+//   var data = {
+//     x: xpos,
+//     y: ypos
+//   }
+
+//   socket.emit('mouse', data);
+// }
